f00017162355fd14438eee91646411cdd9821538
LucasVGS <131281384+LucasGalette@users.noreply.github.com>
2023-06-27 21:11:51 -0300

    Add files via upload

---
+++ Calculo_De_Emissão_De_Co2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Calculo_De_Emissão_De_Co2
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDensidadeGasolina = new System.Windows.Forms.TextBox();
+            this.lblResultado2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLitrosGasolina = new System.Windows.Forms.TextBox();
+            this.lblResul = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Calculo_De_Emissão_De_Co2.Properties.Resources.download_Co2;
+            this.pictureBox1.Location = new System.Drawing.Point(-4, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(804, 505);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(260, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Densidade da Gasolina";
+            // 
+            // txtDensidadeGasolina
+            // 
+            this.txtDensidadeGasolina.Location = new System.Drawing.Point(271, 126);
+            this.txtDensidadeGasolina.Name = "txtDensidadeGasolina";
+            this.txtDensidadeGasolina.Size = new System.Drawing.Size(219, 22);
+            this.txtDensidadeGasolina.TabIndex = 2;
+            // 
+            // lblResultado2
+            // 
+            this.lblResultado2.AutoSize = true;
+            this.lblResultado2.BackColor = System.Drawing.Color.White;
+            this.lblResultado2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado2.Location = new System.Drawing.Point(344, 240);
+            this.lblResultado2.Name = "lblResultado2";
+            this.lblResultado2.Size = new System.Drawing.Size(0, 22);
+            this.lblResultado2.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(286, 295);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(192, 99);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(244, 440);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(271, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(282, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Litros de Gasolina";
+            // 
+            // txtLitrosGasolina
+            // 
+            this.txtLitrosGasolina.Location = new System.Drawing.Point(271, 47);
+            this.txtLitrosGasolina.Name = "txtLitrosGasolina";
+            this.txtLitrosGasolina.Size = new System.Drawing.Size(219, 22);
+            this.txtLitrosGasolina.TabIndex = 8;
+            // 
+            // lblResul
+            // 
+            this.lblResul.AutoSize = true;
+            this.lblResul.BackColor = System.Drawing.Color.White;
+            this.lblResul.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResul.Location = new System.Drawing.Point(331, 199);
+            this.lblResul.Name = "lblResul";
+            this.lblResul.Size = new System.Drawing.Size(0, 22);
+            this.lblResul.TabIndex = 9;
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(801, 502);
+            this.Controls.Add(this.lblResul);
+            this.Controls.Add(this.txtLitrosGasolina);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblResultado2);
+            this.Controls.Add(this.txtDensidadeGasolina);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculo de Co2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDensidadeGasolina;
+        private System.Windows.Forms.Label lblResultado2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLitrosGasolina;
+        private System.Windows.Forms.Label lblResul;
+    }
+}
+
---
+++ Calculo_De_Emissão_De_Co2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_De_Emissão_De_Co2
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double ValorFinal, LitrosGasolina, DensidadeGasolina, x;
+            LitrosGasolina = Convert.ToDouble(txtLitrosGasolina.Text);
+            DensidadeGasolina = Convert.ToDouble(txtDensidadeGasolina.Text);
+            ValorFinal = LitrosGasolina * 0.82;
+            ValorFinal *= DensidadeGasolina * 3.7;
+            for (int i = 0; i < 100; i++)
+            {
+                progressBar1.Value = i+1;
+            }
+            lblResultado2.Text = ValorFinal.ToString();
+            lblResul.Text = "Resultado";
+            progressBar1.Value = 0;
+        }
+    }
+}
